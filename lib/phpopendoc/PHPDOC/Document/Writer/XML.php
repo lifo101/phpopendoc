0b42fb90ea48b8f18d0337892fdac7c3459f39a4
Jason Morriss <lifo101@gmail.com>
2012-06-08 13:13:02 -0400

    updated docblocks. changed <sections> tag into <body>.

--- lib/phpopendoc/PHPDOC/Document/Writer/XML.php
+++ lib/phpopendoc/PHPDOC/Document/Writer/XML.php
@@ -11,15 +11,41 @@ namespace PHPDOC\Document\Writer;
 use PHPDOC\Document,
     PHPDOC\Document\WriterInterface,
     PHPDOC\Document\Writer\Exception\SaveException,
+    PHPDOC\Element\ElementInterface,
     PHPDOC\Element\SectionInterface
     ;
 
+/**
+ * Writer\XML is a class to convert a Document into a basic XML tree.
+ *
+ * This class produces a basic XML tree based on the Document given. One use
+ * for this class is to help with testing and debugging a Document structure.
+ */
 class XML implements WriterInterface 
 {
-    protected $document;
-    protected $dom;
+    /**
+     * @var boolean If true SaveException will be thrown if any errors occur
+     *              during Document processing, otherwise an E_USER_* errors
+     *              will be triggered.
+     */
     protected $throwSaveException;
+
+    /**
+     * @var Document The document to process.
+     */
     
+    protected $document;
+
+    /**
+     * @var \DOMDocument The resulting DOM object.
+     */
+    protected $dom;
+
+    /**
+     * Construct a new instance
+     *
+     * @param Document The document to process. Optional.
+     */
     public function __construct(Document $document = null)
     {
         $this->document = $document;
@@ -28,6 +54,10 @@ class XML implements WriterInterface
 
     /**
      * Saves the document as an XML string.
+     *
+     * @param string   $output   Output filename or other stream.
+     * @param Document $document Document to process. Optional only if a
+     *                           document was given to the constructor.
      */
     public function save($output = null, Document $document = null)
     {
@@ -53,6 +83,8 @@ class XML implements WriterInterface
 
     /**
      * Saves the DOM as XML
+     *
+     * @param string   $output   Output filename or other stream.
      */
     protected function saveDom($output = null)
     {
@@ -81,7 +113,7 @@ class XML implements WriterInterface
      * of their own creation, or pass nothing and a default object will be
      * created instead.
      *
-     * @param \DOMDocument $dom Optional; A new DOMDocument object
+     * @param \DOMDocument $dom Optional new DOMDocument object
      */
     public function setDom(\DOMDocument $dom = null)
     {
@@ -105,7 +137,7 @@ class XML implements WriterInterface
      * save the document.
      *
      * @param Document $document The document to save
-     * @param string $output Filename to output XML to
+     * @param string   $output   The output filename or stream
      */
     public static function saveDocument(Document $document, $output = null)
     {
@@ -113,18 +145,23 @@ class XML implements WriterInterface
         return $writer->save($output, $document);
     }
 
-    public function processDocument($document)
+    /**
+     * Create the DOM from the Document given
+     *
+     * @param Document $document The Document to process
+     */
+    public function processDocument(Document $document)
     {
         // process main Document
         if ($document instanceof Document) {
             $root = $this->dom->createElement('document');
             $this->dom->appendChild($root);
 
-            $sections = $this->dom->createElement('sections');
-            $root->appendChild($sections);
+            $body = $this->dom->createElement('body');
+            $root->appendChild($body);
             
             foreach ($document->getSections() as $section) {
-                $this->processSection($sections, $section);
+                $this->processSection($body, $section);
             }
             
         } else {
@@ -133,6 +170,12 @@ class XML implements WriterInterface
         }
     }
     
+    /**
+     * Process an individual section block
+     *
+     * @param \DOMNode         $node    The DOM node to attach elements to.
+     * @param SectionInterface $section The Section to process
+     */
     public function processSection(\DOMNode $root, SectionInterface $section)
     {
         $node = $this->dom->createElement('section');
@@ -146,9 +189,14 @@ class XML implements WriterInterface
         }
 
         $this->processNode($node, $section);
-     
     }
 
+    /**
+     * Process an individual element
+     *
+     * @param \DOMNode         $node    The DOM node to attach elements to.
+     * @param SectionInterface $section The element to process
+     */
     public function processNode(\DOMNode $root, $element)
     {
         // process all children within the element ...
@@ -175,6 +223,8 @@ class XML implements WriterInterface
      * Configures the writer to throw SaveException on errors.
      *
      * If false trigger_error() is called instead.
+     *
+     * @param bool $value New value.
      */
     public function setSaveException($value)
     {
