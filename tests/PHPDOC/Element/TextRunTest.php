ac771366fbc9c01ff0dc02b16d69175971e5ff17
Jason Morriss <lifo101@gmail.com>
2012-06-08 13:59:08 -0400

    Updated test

--- tests/PHPDOC/Element/TextRunTest.php
+++ tests/PHPDOC/Element/TextRunTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPDOC\Document,
+    PHPDOC\Element\Text,
     PHPDOC\Element\TextRun,
     PHPDOC\Property\Properties
     ;
@@ -26,6 +27,10 @@ class TextRunTest extends \PHPUnit_Framework_TestCase
         $run = new TextRun('test', $prop);
         $this->assertTrue($run->hasProperties(), '->hasProperties() returns true (PropertiesInterface)');
         $this->assertSame($prop, $run->getProperties(), '->getProperties() returns Properties');
+        
+        $text = new Text('test', array('bold' => true));
+        $run = new TextRun($text);
+        $this->assertTrue($run->getProperties()->has('bold'), 'TextRun copied Text properties');
     }
 
 }
