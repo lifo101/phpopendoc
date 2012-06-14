b4939359d75fb032845add44bf51dfd1ed74b6ad
Jason Morriss <lifo101@gmail.com>
2012-06-14 10:26:56 -0400

    Several updates to the element interfaces. Added new ParagraphInterface. Added ElementInterface::getInterface()

---
+++ lib/phpopendoc/PHPDOC/Element/ParagraphInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the PHP Open Doc library.
+ *
+ * @author Jason Morriss <lifo101@gmail.com>
+ * @since  1.0
+ * 
+ */
+namespace PHPDOC\Element;
+
+// @codeCoverageIgnoreStart 
+
+/**
+ * ParagraphInterface defines the interface for a paragraph block.
+ *
+ * @version 1.0
+ * @since 1.0
+ * @author Jason Morriss <lifo101@gmail.com>
+ */
+interface ParagraphInterface
+{
+}
