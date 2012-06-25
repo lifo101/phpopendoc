b8ca80190dc83a052b6e9c44391d3ad84340fa73
Jason Morriss <lifo101@gmail.com>
2012-06-25 11:02:46 -0400

    Added processor for 'border'

--- lib/phpopendoc/PHPDOC/Document/Writer/Word2007/Formatter/TableFormatter.php
+++ lib/phpopendoc/PHPDOC/Document/Writer/Word2007/Formatter/TableFormatter.php
@@ -4,7 +4,7 @@
  *
  * @author Jason Morriss <lifo101@gmail.com>
  * @since  1.0
- * 
+ *
  */
 namespace PHPDOC\Document\Writer\Word2007\Formatter;
 
@@ -18,7 +18,7 @@ use PHPDOC\Element\ElementInterface,
  */
 class TableFormatter extends Shared
 {
-    
+
     /**
      * Property aliases
      */
@@ -26,8 +26,9 @@ class TableFormatter extends Shared
         'align'     => 'jc',
         'justify'   => 'jc',
         'width'     => 'tblW',
+        'border'    => 'tblBorders'
     );
-    
+
     protected function initMap()
     {
         parent::initMap(self::$aliases);
@@ -48,6 +49,69 @@ class TableFormatter extends Shared
             'tblStyleRowBandSize'   => '',
             'tblW'                  => 'tblWidth',
         ) + $this->map;
+    }
+
+    /**
+     * Process border property
+     */
+    protected function process_border($name, $val, ElementInterface $element, \DOMNode $root)
+    {
+        static $sides = array('top', 'right', 'bottom', 'left',
+                              'insideH', 'insideV');
+        static $attrs = array('val', 'color', 'themeColor', 'themeTint',
+                              'themeShade', 'sz', 'space', 'shadow', 'frame');
+
+        $dom = $root->ownerDocument;
+        $prop = $dom->createElement('w:' . $name);
+
+        // If $val is a string then copy its value to each border side
+        if (!is_array($val)) {
+            // @todo make this smarter
+            $val = array(
+                'top' => $val,
+                'right' => $val,
+                'bottom' => $val,
+                'left' => $val,
+                'insideV' => $val,
+                'insideH' => $val
+            );
+        }
+
+        foreach ($val as $side => $bdr) {
+            if (!in_array($side, $sides)) {
+                continue;
+            }
+
+            $node = $dom->createElement('w:' . $side);
+            $prop->appendChild($node);
+
+            // @todo make this smarter
+            if (!is_array($bdr)) {
+                $bdr = array( 'sz' => $bdr );
+            }
+
+            // WordML requires the 'val' attribute to be present
+            if (!isset($bdr['val'])) {
+                $bdr['val'] = 'single';
+            }
+
+            foreach ($bdr as $k => $v) {
+                if (!in_array($k, $attrs)) {
+                    continue;
+                }
+
+                if ($k == 'sz') {
+                    $v = $v * 8;               // Eighths of a point
+                } elseif ($k == 'shadow') {
+                    $v = $this->getOnOff($v);
+                }
+
+                $node->appendChild(new \DOMAttr('w:' . $k, $v));
+            }
+        }
+
+        $root->appendChild($prop);
+        return true;
     }
 
 }
