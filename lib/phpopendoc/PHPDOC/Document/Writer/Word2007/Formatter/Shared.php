4fe369bd24e3496f8c370596d2cd74c9bdeb5966
Jason Morriss <lifo101@gmail.com>
2012-08-13 10:47:26 -0400

    Add 'Table Indent' property support.

--- lib/phpopendoc/PHPDOC/Document/Writer/Word2007/Formatter/Shared.php
+++ lib/phpopendoc/PHPDOC/Document/Writer/Word2007/Formatter/Shared.php
@@ -149,6 +149,11 @@ class Shared
         return true;
     }
 
+    protected function process_tblIndent($name, $val, ElementInterface $element, \DOMNode $root)
+    {
+        return $this->process_tblWidth($name, $val, $element, $root);
+    }
+
     protected function process_tblWidth($name, $val, ElementInterface $element, \DOMNode $root)
     {
         $type = 'dxa';                          // default to twips
